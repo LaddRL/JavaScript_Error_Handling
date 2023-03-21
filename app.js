c2df2349f7b3d06428f736d60c5a45b9fada961e
ladd lindsay <laddrlindsay@gmail.com>
2023-03-21 15:05:37 -0700

    finished exercises

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+  try {
+    return JSON.parse(data); 
+  } catch (err) {
+    console.error(err);
+    return null;
+    // TODO: if an exception is raised
+    // print the error to the console
+    // and return null
+  }
 }
 
-let failData = "<h1>Hello World!</h1>"
-
+let failData = "<h1>Hello World!</h1>";
+let correctedData = '{"sentence" : "Hello World!"}';
 let passData = JSON.stringify({ success: true });
 
 console.log(parseToJSON(failData));
-
+console.log(parseToJSON(correctedData));
+console.log(parseToJSON(test));
 console.log(parseToJSON(passData));
